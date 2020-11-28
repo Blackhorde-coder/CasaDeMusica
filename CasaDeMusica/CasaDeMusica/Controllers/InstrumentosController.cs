387dd7b0f64d1a9ea5bc80d4d9e3f70547ce05f6
Blackhorde <esingcomputacion@gmail.com>
2020-11-28 19:17:52 -0300

    Editar

--- CasaDeMusica/CasaDeMusica/Controllers/InstrumentosController.cs
+++ CasaDeMusica/CasaDeMusica/Controllers/InstrumentosController.cs
@@ -26,8 +26,32 @@ namespace CasaDeMusica.Controllers
             GestorDatos.GestorDatos gestor = new GestorDatos.GestorDatos();
             gestor.agregarinstrumento(dto.I);
             List<Instrumento> lista = gestor.listadeinstrumentos();
+            return View("Listado", lista);
+        }
+        public ActionResult Listado()
+        {
+            GestorDatos.GestorDatos gestor = new GestorDatos.GestorDatos();
+            List<Instrumento> lista = gestor.listadeinstrumentos();
+            return View(lista);
+        }
+        public ActionResult Editar(int idinstrumento)
+        {
+            GestorDatos.GestorDatos gestor = new GestorDatos.GestorDatos();
+            Instrumento i = gestor.buscarinstrumento(idinstrumento);
+            DTOinstrumento dto = new DTOinstrumento();
+            dto.I = i;
+            dto.Listatipos = gestor.listadetipos();
+            return View(dto);
+        }
+        [HttpPost]
+        public ActionResult Editar(DTOinstrumento dto)
+        {
+            GestorDatos.GestorDatos gestor = new GestorDatos.GestorDatos();
+            gestor.actualizarinstrumento(dto.I);
+            List<Instrumento> lista = gestor.listadeinstrumentos();
+            return View("Listado", lista);
 
-            return View();
+
         }
     }
 }
--- CasaDeMusica/CasaDeMusica/GestorDatos/GestorDatos.cs
+++ CasaDeMusica/CasaDeMusica/GestorDatos/GestorDatos.cs
@@ -25,7 +25,7 @@ namespace CasaDeMusica.GestorDatos
             con.ConnectionString = @"Data Source=BLACKHORDENOT;Initial Catalog=casademusica;Integrated Security=True";
             try
             {
-                string consultasql = @"insert into instrumentos(nombre, descripcion, stock, precio, idTipo) value(@nombre, @descpicion, @stock, @precio, @idtipo)";
+                string consultasql = @"insert into instrumentos(nombre, descripcion, stock, precio, idTipo) values(@nombre, @descripcion, @stock, @precio, @idtipo)";
                 con.Open();
                 cmd = new SqlCommand(consultasql, con);
 
@@ -67,7 +67,7 @@ namespace CasaDeMusica.GestorDatos
         public List<Instrumento> listadeinstrumentos()
         {
             con.ConnectionString = @"Data Source=BLACKHORDENOT;Initial Catalog=casademusica;Integrated Security=True";
-            string consultasql = @"select * from tipos";
+            string consultasql = @"select * from instrumentos";
             con.Open();
             cmd = new SqlCommand(consultasql, con);
             dr = cmd.ExecuteReader();
@@ -78,7 +78,7 @@ namespace CasaDeMusica.GestorDatos
                 i.Idinstrumento = int.Parse(dr["idInstrumento"].ToString());
                 i.Nombre = dr["nombre"].ToString();
                 i.Descripcion = dr["descripcion"].ToString();
-                i.Stock = int.Parse(dr["sotck"].ToString());
+                i.Stock = int.Parse(dr["stock"].ToString());
                 i.Precio = double.Parse(dr["precio"].ToString());
                 i.Idtipo = int.Parse(dr["idTipo"].ToString());
                 lista.Add(i);
@@ -86,6 +86,48 @@ namespace CasaDeMusica.GestorDatos
             dr.Close();
             con.Close();
             return lista;
+        }
+        public Instrumento buscarinstrumento(int idinstrumento)
+        {
+            con.ConnectionString = @"Data Source=BLACKHORDENOT;Initial Catalog=casademusica;Integrated Security=True";
+            string consultasql = @"select * from instrumentos where idInstrumento = @idinstrumento";
+            con.Open();
+            cmd = new SqlCommand(consultasql, con);
+            cmd.Parameters.AddWithValue("@idinstrumento", idinstrumento);
+            dr = cmd.ExecuteReader();
+            dr.Read();
+            Instrumento i = new Instrumento();
+            i.Idinstrumento = int.Parse(dr["idInstrumento"].ToString());
+            i.Nombre = dr["nombre"].ToString();
+            i.Descripcion = dr["descripcion"].ToString();
+            i.Stock = int.Parse(dr["stock"].ToString());
+            i.Precio = double.Parse(dr["precio"].ToString());
+            i.Idtipo = int.Parse(dr["idTipo"].ToString());
+            dr.Close();
+            con.Close();
+            return i;
+        }
+        public void actualizarinstrumento(Instrumento i)
+        {
+            con.ConnectionString = @"Data Source=BLACKHORDENOT;Initial Catalog=casademusica;Integrated Security=True";
+            string consultasql = @"update instrumentos
+                                    set nombre=@nombre,
+	                                    descripcion=@descripcion,
+	                                    stock=@stock,
+	                                    precio=@precio,
+	                                    idtipo=@idtipo
+	                                    where idInstrumento=@idintrumento";
+            con.Open();
+            cmd = new SqlCommand(consultasql, con);
+            cmd.Parameters.AddWithValue("@nombre", i.Nombre);
+            cmd.Parameters.AddWithValue("@descripcion", i.Descripcion);
+            cmd.Parameters.AddWithValue("@stock", i.Stock);
+            cmd.Parameters.AddWithValue("@precio", i.Precio);
+            cmd.Parameters.AddWithValue("@idtipo", i.Idtipo);
+            cmd.Parameters.AddWithValue("@idintrumento", i.Idinstrumento);
+            cmd.ExecuteNonQuery();
+            con.Close();
+
         }
     }
 }
